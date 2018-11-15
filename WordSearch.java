07d0fd298afd3f7d0d708ecc532dbd2b930d6ca7
cyee00 <cyee00@stuy.edu>
2018-11-15 10:16:22 -0500

    w

--- WordSearch.java
+++ WordSearch.java
@@ -192,12 +192,74 @@ public WordSearch( int rows, int cols, String fileName, int randSeed) throws Fil
   //  Both will read in the word text file, then run addAllWords(). Do not fill in random letters after.
 
 //Two Methods
-//private boolean addWord( int r, int c, String word, int rowIncrement, int colIncrement)
+private boolean addWord( int row, int col, String word, int rowIncrement, int colIncrement){
+  boolean modify = false;
+  word = word.toUpperCase();
+  if (rowIncrement==0&&colIncrement==0){
+    return false;
+  }
+  for (int i=0;i<word.length();i++){
+    if (word.charAt(i)==data[row+(rowIncrement*i)][col+(colIncrement*i)]||
+    data[row+(rowIncrement*i)][col+(colIncrement*i)]=='_'){
+      modify = true;
+    }
+  }
+  if (modify){
+    for (int i=0;i<word.length();i++){
+      data[row+(rowIncrement*i)][col+(colIncrement*i)]=word.charAt(i);
+    }
+  }
+  return false;
+}
+/**Attempts to add a given word to the specified position of the WordGrid.
+  *The word is added in the direction rowIncrement,colIncrement
+
+  *Words must have a corresponding letter to match any letters that it overlaps.
+
+  *
+  *@param word is any text to be added to the word grid.
+  *@param row is the vertical locaiton of where you want the word to start.
+  *@param col is the horizontal location of where you want the word to start.
+
+  *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+  *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+
+  *@return true when: the word is added successfully.
+  *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+  *        OR there are overlapping letters that do not match
+  */
     /*-when colIncrement and rowIncrement are both 0, return false.
     -when you successfully add a word, move the word from wordsToAdd to wordsAdded, then return true.
     -return false otherwise. */
+    /*[rowIncrement,colIncrement] examples:
 
-//private boolean addAllWords() {}
+        *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+
+        *[ 1,0] would add downwards because (row+1), with no col change
+
+        *[ 0,-1] would add towards the left because (col - 1), with no row change
+
+        */
+private boolean addAllWords() {
+  int tries = 1000;
+  while (tries>0){//stops trying to add any words when tries reaches 0, i.e. 999 tries
+    randgen = new Random();
+    String word = wordsToAdd.get(randgen.nextInt()%wordsToAdd.size());
+    int rowIncrement = randgen.nextInt()%2;
+    int colIncrement = randgen.nextInt()%2;
+    for (int i=0;i<100;i++){ //run out of positional tries at 100, so move on to the next word
+      int row=Math.abs(randgen.nextInt()%data.length);
+      int col=Math.abs(randgen.nextInt()%data[0].length);
+      if (addWord(row,col,word,rowIncrement,colIncrement)){
+        i=100; //word added successfully, so move on to the next word
+      }
+    }
+    if (wordsToAdd.isEmpty()){//all words added, so stop
+      return true;
+    } else {tries--;}//else keep going
+  }
+  return true;
+}
       /*-Attempt to add all of the words from the wordsToAdd list using the following algorithm:
     1-Choose a random word, and a random direction (rowIncrement/colIncrement)
     2-Trying to add that word to different starting positions until:
